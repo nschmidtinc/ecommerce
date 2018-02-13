c04008f997899fe3778f7a67cad7c6686725f831
Alexandre <alex.f.april@hotmail.com>
2018-02-13 10:26:26 -0500

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,19 +1,17 @@
+var alibay = require('./alibay');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.raw({ type: "*/*" }));
 var fs = require('fs');
+
 var passwords = {};
 var items = {};
 
 try {
     passwords = JSON.parse(fs.readFileSync("users.json"));
     items = JSON.parse(fs.readFileSync("test.json"));
-} catch (err) { console.log("No list exists"); }
-
-app.get('/listAllItems', (req, res) => {
-    res.send(JSON.stringify(items));
-});
+} catch (err) { console.log("No list exist"); }
 
 app.post('/signup', (req, res) => {
     console.log("in the server");
@@ -39,59 +37,20 @@ app.post('/login', (req, res) => {
     else if (passwords[json.username] !== json.password) { return res.send("Incorrect password"); }
     else if (passwords[json.username] === json.password) { res.send("Log in successful"); }
 });
-app.listen(4000);
 
-// {
-//     "User 1": {
-//         "Item 1 ID": {
-//             "Item Properties": {
-//                 "Price": "10",
-//                 "Description": "User 1, Item 1"
-//             }
-//         },
-//         "Item 2 ID": {
-//             "Item Properties": {
-//                 "Price": "20",
-//                 "Description": "User 1, Item 2"
-//             }
-//         },
-//         "Item 3 ID": {
-//             "Item Properties": {
-//                 "Price": "30",
-//                 "Description": "User 1, Item 3"
-//             }
-//         }
-//     },
-//     "User 2": {
-//         "Item 1 ID": {
-//             "Item Properties": {
-//                 "Price": "40",
-//                 "Description": "User 2, Item 1"
-//             }
-//         },
-//         "Item 2 ID": {
-//             "Item Properties": {
-//                 "Price": "50",
-//                 "Description": "User 2, Item 2"
-//             }
-//         },
-//         "Item 3 ID": {
-//             "Item Properties": {
-//                 "Price": "60",
-//                 "Description": "User 2, Item 3"
-//             }
-//         },
-//         "Item 4 ID": {
-//             "Item Properties": {
-//                 "Price": "70",
-//                 "Description": "User 2, Item 4"
-//             }
-//         },
-//         "Item 5 ID": {
-//             "Item Properties": {
-//                 "Price": "80",
-//                 "Description": "User 2, Item 5"
-//             }
-//         }
-//     }
-// }
+app.get('/itemBought', (req, res) => {
+    var userID = req.query.userID;
+    res.send(JSON.stringify(alibay.getItemsBought(userID)));
+});
+
+app.get('/listAllItems', (req, res) => {
+    res.send(JSON.stringify(items));
+});
+app.post('/newListing', (req, res) => {
+    let payload = JSON.parse(req.body.toString());
+    let sellerID = payload.userID;
+    let price = payload.price;
+    let description = payload.description;
+    createListing(sellerID, price, description);
+});
+app.listen(4000);
